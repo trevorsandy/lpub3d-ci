276345b1f0c0066e8dc284a13ae74ae67914f30d
Trevor Sandy <trevor.sandy@gmail.com>
2017-10-26 06:35:22 +0200

    Docker PKG container build - 23 [ci skip]

--- builds/linux/CreateRpm.sh
+++ builds/linux/CreateRpm.sh
@@ -108,21 +108,16 @@ echo "10. build the RPM package..."
 # check the spec
 rpmlint ${LPUB3D}.spec
 
-# this seems to be the build command for OpenSUSE...
 #rpmbuild --define "_topdir ${WORK_DIR}/rpmbuild" -v -ba ${LPUB3D}.spec
-# this looks to be the build command for Fedora...
 rpmbuild --define "_topdir ${RPM_BUILD_DIR}" -vv -ba ${LPUB3D}.spec
-# check the RPMs
-rpmlint ../RPMS/*/${LPUB3D}*.rpm ../SRPMS/${LPUB3D}*.rpm
-
-# debug
-rpm-temp-logs=`ls ~/rpm/tmp/rpm-tmp.*`
-for FILE in ${rpm-temp-logs}; do echo "*** DEBUG File: $FILE" && cat $FILE; done
 
 cd ../RPMS/${LP3D_TARGET_ARCH}
 DISTRO_FILE=`ls ${LPUB3D}*.rpm`
 if [ -f ${DISTRO_FILE} ] && [ ! -z ${DISTRO_FILE} ]
 then
+    echo "11a. check rpm packages..."
+    rpmlint ${DISTRO_FILE} ../../SRPMS/${LPUB3D}*.rpm
+
     echo "11. create update and download packages..."
     IFS=- read NAME RPM_VERSION RPM_EXTENSION <<< ${DISTRO_FILE}
 
@@ -139,6 +134,18 @@ echo "12. cleanup cloned ${LPUB3D} repository from SOURCES/ and BUILD/..."
 rm -rf ../../SOURCES/${WORK_DIR} ../../BUILD/${WORK_DIR}
 
 echo " DEBUG Package files:" `ls ../RPMS`
+
+# debug
+echo "********** RPM-TMP Logs ******************"
+rpm-temp-logs=
+if [ -d "~/rpm/tmp" ]; then
+    rpm-temp-logs=`ls ~/rpm/tmp/rpm-tmp*`
+fi
+if [ -d "/var/tmp" ]; then
+    rpm-temp-logs=${rpm-temp-logs} `ls /var/tmp/rpm-tmp*`
+fi
+for FILE in ${rpm-temp-logs}; do echo "* rpm-tmp log: $FILE" && cat $FILE; done
+echo
 #echo " DEBUG Package files: `find $PWD`"
 echo "$ME Finished!"
 #mv $LOG "${CWD}/rpmbuild/$ME.log"
