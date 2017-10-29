929b6dfd106d5668455f4554d0543f894925ff83
Trevor Sandy <trevor.sandy@gmail.com>
2017-10-29 17:50:11 +0100

    Docker PKG container build - 96 [ci skip]

--- builds/linux/CreateRpm.sh
+++ builds/linux/CreateRpm.sh
@@ -42,7 +42,7 @@ then
     mkdir rpmbuild
 fi
 cd rpmbuild
-RPM_BUILD_DIR=$PWD
+BUILD_DIR=$PWD
 for DIR in {BUILD,RPMS,SRPMS,SOURCES,SPECS}
 do
     if [ ! -d "${DIR}" ]
@@ -50,7 +50,7 @@ do
         mkdir "${DIR}"
     fi
 done
-cd ${RPM_BUILD_DIR}/SOURCES
+cd ${BUILD_DIR}/SOURCES
 
 echo "2. download ${LPUB3D} source to SOURCES/..."
 git clone https://github.com/trevorsandy/${LPUB3D}.git
@@ -68,13 +68,12 @@ cp -f ${WORK_DIR}/mainApp/images/lpub3d.xpm .
 if [ -f "lpub3d.xpm" ]; then echo "   DEBUG lpub3d.xpm copied"; else echo "   DEBUG lpub3d.xpm not found!"; fi
 
 echo "7. copy ${LPUB3D}.spec to SPECS/"
-cp -f ${WORK_DIR}/builds/linux/obs/${LPUB3D}.spec ${RPM_BUILD_DIR}/SPECS
-if [ -f "../SPECS/${LPUB3D}.spec" ]; then echo "   DEBUG ${LPUB3D}.spec copied"; else echo "   DEBUG ${LPUB3D}.spec not found!"; fi
-source /etc/os-release && if [ "$ID" = "fedora" ]; then sed 's/Icon: lpub3d.xpm/# Icon: lpub3d.xpm remarked - fedora does not like/' -i ${LPUB3D}.spec; fi
+cp -f ${WORK_DIR}/builds/linux/obs/${LPUB3D}.spec ${BUILD_DIR}/SPECS
+if [ -f "${BUILD_DIR}/SPECS/${LPUB3D}.spec" ]; then echo "   DEBUG ${LPUB3D}.spec copied"; else echo "   DEBUG ${LPUB3D}.spec not found!"; fi
 
 echo "8. copy ${LPUB3D}-rpmlintrc to SPECS/"
-cp -f ${WORK_DIR}/builds/linux/obs/${LPUB3D}-rpmlintrc ${RPM_BUILD_DIR}/SPECS
-if [ -f "../SPECS/${LPUB3D}-rpmlintrc" ]; then echo "   DEBUG ${LPUB3D}-rpmlintrc copied"; else echo "   DEBUG ${LPUB3D}-rpmlintrc not found!"; fi
+cp -f ${WORK_DIR}/builds/linux/obs/${LPUB3D}-rpmlintrc ${BUILD_DIR}/SPECS
+if [ -f "${BUILD_DIR}/SPECS/${LPUB3D}-rpmlintrc" ]; then echo "   DEBUG ${LPUB3D}-rpmlintrc copied"; else echo "   DEBUG ${LPUB3D}-rpmlintrc not found!"; fi
 
 echo "9. download LDraw archive libraries to SOURCES/..."
 if [ ! -f lpub3dldrawunf.zip ]
@@ -106,9 +105,10 @@ tar -czf ${WORK_DIR}.tar.gz \
         --exclude="${WORK_DIR}/.gitignore" \
         --exclude="${WORK_DIR}/appveyor.yml" ${WORK_DIR}
 
-cd ${RPM_BUILD_DIR}/SPECS
+cd ${BUILD_DIR}/SPECS
 echo "12. download build dependenvies..."
 if [ -f "${LPUB3D}.spec" ]; then echo "   DEBUG ${LPUB3D}.spec exist at $PWD" ; cat ${LPUB3D}.spec; else echo "   DEBUG ${LPUB3D}.spec not found at at $PWD!"; fi
+source /etc/os-release && if [ "$ID" = "fedora" ]; then sed 's/Icon: lpub3d.xpm/# Icon: lpub3d.xpm remarked - fedora does not like/' -i "${BUILD_DIR}/SPECS/${LPUB3D}.spec"; fi
 dnf builddep -y ${LPUB3D}.spec
 
 echo "13. build the RPM package..."
@@ -116,14 +116,14 @@ echo "13. build the RPM package..."
 rpmlint ${LPUB3D}.spec
 
 #rpmbuild --define "_topdir ${WORK_DIR}/rpmbuild" -v -ba ${LPUB3D}.spec
-rpmbuild --define "_topdir ${RPM_BUILD_DIR}" -vv -ba ${LPUB3D}.spec
+rpmbuild --define "_topdir ${BUILD_DIR}" -vv -ba ${LPUB3D}.spec
 
-cd ${RPM_BUILD_DIR}/RPMS/${LP3D_TARGET_ARCH}
+cd ${BUILD_DIR}/RPMS/${LP3D_TARGET_ARCH}
 DISTRO_FILE=`ls ${LPUB3D}*.rpm`
 if [ -f ${DISTRO_FILE} ] && [ ! -z ${DISTRO_FILE} ]
 then
     echo "14. check rpm packages..."
-    rpmlint ${DISTRO_FILE} ${RPM_BUILD_DIR}/SRPMS/${LPUB3D}*.rpm
+    rpmlint ${DISTRO_FILE} ${BUILD_DIR}/SRPMS/${LPUB3D}*.rpm
 
     echo "15. create update and download packages..."
     IFS=- read NAME RPM_VERSION RPM_EXTENSION <<< ${DISTRO_FILE}
@@ -138,9 +138,9 @@ else
 fi
 
 echo "15. cleanup cloned ${LPUB3D} repository from SOURCES/ and BUILD/..."
-rm -rf ${RPM_BUILD_DIR}/SOURCES/${WORK_DIR} ${RPM_BUILD_DIR}/BUILD/${WORK_DIR}
+rm -rf ${BUILD_DIR}/SOURCES/${WORK_DIR} ${BUILD_DIR}/BUILD/${WORK_DIR}
 
-echo " DEBUG Package files:" `ls ${RPM_BUILD_DIR}/RPMS`
+echo " DEBUG Package files:" `ls ${BUILD_DIR}/RPMS`
 echo
 #echo " DEBUG Package files: `find $PWD`"
 echo "$ME Finished!"
