a6e7f6d4dbacedb143d5c5c6b3d5bbe0583cc0e5
Trevor SANDY <trevor.sandy@gmail.com>
2018-01-16 14:03:47 +0100

    Update workind directory after get Qt [skip ci]

--- builds/utilities/GetQt5Libs.sh
+++ builds/utilities/GetQt5Libs.sh
@@ -11,9 +11,11 @@
 # sample command [call from root build directory - e.g. lpub3d/]:
 # chmod a+x builds/utilities/GetQt5Libs.sh && ./builds/utilities/GetQt5Libs.sh
 
+# Set working directory
+LP3D_OBS_WD=$PWD; LP3D_QT_WD=$(cd ../ && echo $PWD)
+
 # Extract QtBinPatcher
-LP3D_WD=$(cd ../ && echo $PWD) && \
-LP3D_BINPATCHER="QtBinPatcher-2.2.1" && cd $LP3D_WD && \
+LP3D_BINPATCHER="QtBinPatcher-2.2.1" && cd $LP3D_QT_WD && \
 tar -xzf ${LP3D_BINPATCHER}.tar.gz && cd $LP3D_BINPATCHER && \
 mkdir -p build; cd build || echo "ERROR - Could not extract QtBinPatcher!"
 
@@ -21,7 +23,7 @@ mkdir -p build; cd build || echo "ERROR - Could not extract QtBinPatcher!"
 cmake -G"Unix Makefiles" \
 -DCMAKE_INSTALL_PREFIX=$(cd ../ && echo $PWD) \
 -DCMAKE_BUILD_TYPE=Release $(cd ../ && echo $PWD) && \
-make && make install && cd $LP3D_WD || echo "ERROR - Could not build QtBinPatcher!"
+make && make install && cd $LP3D_QT_WD || echo "ERROR - Could not build QtBinPatcher!"
 
 # Extract Qt library
 LP3D_QTLIBS=qt5-5.9.3-gcc_64-rhel && \
@@ -34,3 +36,6 @@ cp $LP3D_BINPATCHER/bin/qtbinpatcher Qt/5.9.3/gcc_64/bin && \
 cd Qt/5.9.3/gcc_64/bin && ./qtbinpatcher --verbose --nobackup &&
 export LP3D_QT5_BIN=$PWD && echo "LP3D_QT5_BIN: $LP3D_QT5_BIN" || \
 echo "ERROR - Could not copy and run QtBinPatcher!"
+
+# Restore working directory
+cd $LP3D_OBS_WD && echo "Return to working directory: $LP3D_OBS_WD"
