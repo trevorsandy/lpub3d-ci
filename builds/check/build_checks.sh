fe4c387572e365c638950ff9a3b1b8ba668ecc30
Trevor SANDY <trevor.sandy@gmail.com>
2018-07-10 10:37:05 +0200

    Dev - Refactor Unix build check script

--- builds/check/build_checks.sh
+++ builds/check/build_checks.sh
@@ -6,28 +6,36 @@
 
 if [[ "${DOCKER}" = "true" && ${XMING} != "true" ]]; then
 echo && echo "- Using XVFB"
-LP3D_XVFB_CMD="xvfb-run --auto-servernum --server-num=1 --server-args=\"-screen 0 1024x768x24\""
-else
-LP3D_XVFB_CMD=""
+USE_XVFB="true"
 fi
 
 LP3D_CHECK_FILE="${PWD}/${SOURCE_DIR}/builds/check/build_checks.mpd"
 
-echo && echo "- Check 1 of 4: file process check..."
-LP3D_CHECK_OPTIONS="--ignore-console-redirect --process-file --preferred-renderer native"
-${LP3D_XVFB_CMD} ${LPUB3D_EXE} $LP3D_CHECK_OPTIONS $LP3D_CHECK_FILE
+for LP3D_BUILD_CHECK in CHECK01 CHECK02 CHECK03 CHECK04; do
+    case ${LP3D_BUILD_CHECK} in
+    CHECK01)
+        LP3D_CHECK_HDR="- Check 1 of 4: Native File Process Check..."
+        LP3D_CHECK_OPTIONS="--process-file --preferred-renderer native"
+        ;;
+    CHECK02)
+        LP3D_CHECK_HDR="- Check 2 of 4: LDView File Process Check..."
+        LP3D_CHECK_OPTIONS="--process-file --clear-cache --preferred-renderer ldview"
+        ;;
+    CHECK03)
+        LP3D_CHECK_HDR="- Check 3 of 4: LDGLite Export Range Check..."
+        LP3D_CHECK_OPTIONS="--process-export --range 1-3 --clear-cache --preferred-renderer ldglite"
+        ;;
+    CHECK04)
+        LP3D_CHECK_HDR="- Check 4 of 4: Native POV Generation Check..."
+        LP3D_CHECK_OPTIONS="--process-file --clear-cache --preferred-renderer povray"
+        ;;
+      esac
 
-echo && echo "- Check 2 of 4: export check..."
-LP3D_CHECK_OPTIONS="--ignore-console-redirect --process-file --clear-cache --preferred-renderer ldview"
-${LP3D_XVFB_CMD} ${LPUB3D_EXE} $LP3D_CHECK_OPTIONS $LP3D_CHECK_FILE
-
-echo && echo "- Check 3 of 4: range check..."
-LP3D_CHECK_OPTIONS="--ignore-console-redirect --process-export --range 1-3 --clear-cache --preferred-renderer ldglite"
-${LP3D_XVFB_CMD} ${LPUB3D_EXE} $LP3D_CHECK_OPTIONS $LP3D_CHECK_FILE
-
-echo && echo "- Check 4 of 4: povray check..."
-LP3D_CHECK_OPTIONS="--ignore-console-redirect --process-file --clear-cache --preferred-renderer povray"
-${LP3D_XVFB_CMD} ${LPUB3D_EXE} $LP3D_CHECK_OPTIONS $LP3D_CHECK_FILE
+    echo && echo ${LP3D_CHECK_HDR}
+    [ -n "$USE_XVFB" ] && xvfb-run --auto-servernum --server-num=1 --server-args="-screen 0 1024x768x24" \
+    ${LPUB3D_EXE} ${LP3D_CHECK_OPTIONS} ${LP3D_CHECK_FILE} || \
+    ${LPUB3D_EXE} ${LP3D_CHECK_OPTIONS} ${LP3D_CHECK_FILE}
+done
 
 echo "------------Build checks completed----------" && echo
 
