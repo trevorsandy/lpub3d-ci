28da5ff14035a1bb591298efa8884843b6d0e1fe
Trevor SANDY <trevor.sandy@gmail.com>
2025-06-21 20:46:26 +0200

    Qt6 - QTextStream left to Qt::left compatibility update

--- mainApp/threadworkers.cpp
+++ mainApp/threadworkers.cpp
@@ -1825,12 +1825,24 @@ void ColourPartListWorker::writeLDrawColourPartFile(bool append) {
                 QString partNumber      = cpLine.section(":::",0,0);
                 QString library         = cpLine.section(":::",1,1);
                 QString partDescription = cpLine.section(":::",2,2);
-                out << left << qSetFieldWidth(_colWidthFileName+1)    << partNumber
+                out <<
+#if QT_VERSION >= QT_VERSION_CHECK(5,15,0)
+                       Qt::left
+#else
+                       left
+#endif
+                            << qSetFieldWidth(_colWidthFileName+1)    << partNumber
                             << qSetFieldWidth(9) << library
                             << partDescription   << qSetFieldWidth(0) << lpub_endl;
 
             } else if (cpLine.section(":::",0,0) == QLatin1String("# File Name")) {
-                out << left << qSetFieldWidth(_colWidthFileName+1)    << cpLine.section(":::",0,0)
+                out <<
+#if QT_VERSION >= QT_VERSION_CHECK(5,15,0)
+                       Qt::left
+#else
+                       left
+#endif
+                            << qSetFieldWidth(_colWidthFileName+1)    << cpLine.section(":::",0,0)
                             << qSetFieldWidth(9)           << cpLine.section(":::",1,1)
                             << cpLine.section(":::",2,2)   << qSetFieldWidth(0) << lpub_endl;
 
