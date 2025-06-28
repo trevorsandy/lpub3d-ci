3661917d61b857ecb1a6a27d23b226b7a927dda6
Trevor SANDY <trevor.sandy@gmail.com>
2025-06-28 20:28:20 +0200

    LCLib: add ladder widget to float properties.

--- mainApp/version.h
+++ mainApp/version.h
@@ -233,7 +233,7 @@
 // ~~~~~~ LeoCAD Version ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 #define VER_LC_VERSION_TEXT                 "23.03"
-#define VER_LC_VERSION_SHA                  "ed24c15"
+#define VER_LC_VERSION_SHA                  "f760907"
 
 // ~~~~~~ Build Parameters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
