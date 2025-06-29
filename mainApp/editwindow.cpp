d7b2740d9c5347385e79719fa1d4f196c3944532
Trevor SANDY <trevor.sandy@gmail.com>
2025-06-29 17:20:12 +0200

    W Qt6 - QString reference editwindow compatibility update

--- mainApp/editwindow.cpp
+++ mainApp/editwindow.cpp
@@ -1073,11 +1073,7 @@ int EditWindow::setCurrentStep(const int lineNumber, bool inScope)
 
     if (!stepKey.isEmpty()) {
         Step *currentStep = lpub->currentStep;
-#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
         if (currentStep && currentStep->viewerStepKey.startsWith(stepKey))
-#else
-        if (currentStep && currentStep->viewerStepKey.startsWith(&stepKey))
-#endif
             return EXISTING_CURRENT_STEP;
 
         // set current step
