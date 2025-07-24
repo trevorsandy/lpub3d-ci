1bea8446785ea4dfced88b1f5cf5f6bb4a93f9ea
Trevor SANDY <trevor.sandy@gmail.com>
2025-07-24 17:04:42 +0200

    Qt6 - packaging, set Git folder

--- builds/windows/AutoBuild.bat
+++ builds/windows/AutoBuild.bat
@@ -363,6 +363,14 @@ IF NOT EXIST "%LP3D_WIN_TAR%" (
   SET "LP3D_WIN_TAR_MSG=%LP3D_WIN_TAR%"
 )
 
+rem Set GIT path
+IF "%LP3D_WIN_GIT%" == "" SET "LP3D_WIN_GIT=%ProgramFiles%\Git\cmd"
+IF NOT EXIST "%LP3D_WIN_GIT%" (
+  IF "%LP3D_WIN_GIT_MSG%" == "" SET LP3D_WIN_GIT_MSG=Not Found
+) ELSE (
+  SET "LP3D_WIN_GIT_MSG=%LP3D_WIN_GIT%"
+)
+
 rem Initialize package install to - no install
 IF [%2]==[] (
   SET PKG_INSTALL=0
@@ -406,7 +414,6 @@ IF "%BUILD_WORKER%" EQU "True" (
   )
   ECHO   BUILD_WORKER_OS................[%BUILD_WORKER_OS%]
   ECHO   PROJECT REPOSITORY.............[%BUILD_WORKER_REPO%]
-  ECHO   LP3D_WIN_GIT_DIR...............[%LP3D_WIN_GIT_MSG%]
 )
 IF "%APPVEYOR%" EQU "True" (
   ECHO   BUILD_HOST.....................[APPVEYOR CONTINUOUS INTEGRATION SERVICE]
@@ -416,7 +423,6 @@ IF "%APPVEYOR%" EQU "True" (
   ECHO   PROJECT_NAME...................[%APPVEYOR_PROJECT_NAME%]
   ECHO   REPOSITORY_NAME................[%APPVEYOR_REPO_NAME%]
   ECHO   REPO_PROVIDER..................[%APPVEYOR_REPO_PROVIDER%]
-  ECHO   LP3D_WIN_GIT_DIR...............[%LP3D_WIN_GIT_MSG%]
 )
 ECHO   PACKAGE........................[%PACKAGE%]
 ECHO   CONFIGURATION..................[%CONFIGURATION%]
@@ -429,14 +435,10 @@ ECHO   LDRAW_DIRECTORY................[%LDRAW_DIR%]
 ECHO   LDRAW_INSTALL_ROOT.............[%LDRAW_INSTALL_ROOT%]
 ECHO   LDRAW_LIBS_ROOT................[%LDRAW_LIBS%]
 ECHO   BUILD_OPT......................[%BUILD_OPT%]
+ECHO   LP3D_WIN_GIT_DIR...............[%LP3D_WIN_GIT_MSG%]
 ECHO   LP3D_WIN_TAR...................[%LP3D_WIN_TAR_MSG%]
 IF %LP3D_AMD64_ARM64_CROSS% EQU 1 (
 ECHO   COMPILATION....................[ARM64 ON AMD64 HOST]
-)
-
-IF NOT EXIST "%LP3D_WIN_GIT%" (
-  SET LP3D_WIN_GIT=
-  SET LP3D_WIN_GIT_MSG=Not Found
 )
 
 IF /I "%RENDERERS_ONLY%"=="1" (
