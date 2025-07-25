80bf9ea23851f9507c2262a1cfa2bd7f24df6783
Trevor SANDY <trevor.sandy@gmail.com>
2025-07-25 05:19:50 +0200

    LCLib: fixed gcc error.

--- lclib/common/lc_global.h
+++ lclib/common/lc_global.h
@@ -71,7 +71,7 @@ char* strupr(char* string);
 #define LC_VERSION_PATCH 0
 #define LC_VERSION_TEXT "23.03"
 /*** LPub3D Mod - LeoCAD Git SHA ***/
-#define LC_VERSION_SHA "01d54ad"
+#define LC_VERSION_SHA "19464de"
 /*** LPub3D Mod end ***/
 
 
