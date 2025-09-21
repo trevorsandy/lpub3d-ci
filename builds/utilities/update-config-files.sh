fe962ff1a8d3c7e1d15dad91b3302ec1a45ad661
Trevor SANDY <trevor.sandy@gmail.com>
2025-09-21 05:08:28 +0200

    Builds - set default committer email

--- builds/utilities/update-config-files.sh
+++ builds/utilities/update-config-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Trevor SANDY
-# Last Update September 17, 2025
+# Last Update September 21, 2025
 # Copyright (C) 2016 - 2025 by Trevor SANDY
 #
 # This script is automatically executed by qmake from mainApp.pro
@@ -114,8 +114,6 @@ then
         fi
         git fetch -qfup --depth=${LP3D_GIT_DEPTH} origin +${LP3D_BRANCH} +refs/tags/*:refs/tags/* >/dev/null 2>&1
         git checkout -qf ${LP3D_COMMIT} >/dev/null 2>&1
-        lp3d_git_ver_author="$(git log -1 ${LP3D_COMMIT} --pretty="%aN")" >/dev/null 2>&1
-        lp3d_git_ver_committer_email="$(git log -1 ${LP3D_COMMIT} --pretty="%cE")" >/dev/null 2>&1
     else
         Info "$((LP3D_CMD_COUNT += 1)). capture version info using git queries"
     fi
@@ -133,8 +131,6 @@ then
     if test -z "$lp3d_git_build_type"; then LP3D_BUILD_TYPE="Continuous"; else LP3D_BUILD_TYPE="Release"; fi
     if test "$lp3d_ver_tmp" != "$lp3d_version_"; then lp3d_suffix=${lp3d_ver_tmp}; fi # check if ver_tmp not same as version_ - suffix exist
     if test -n "$lp3d_suffix"; then lp3d_version_=${lp3d_version_%_*}; fi             # remove everything after and including "_" - suffix exist
-    if test -n "$lp3d_git_ver_author"; then LP3D_AUTHOR_NAME=${lp3d_git_ver_author}; else LP3D_AUTHOR_NAME=`echo $USER`; fi
-    if test -n "$lp3d_git_ver_committer_email"; then LP3D_COMMITTER_EMAIL=${lp3d_git_ver_committer_email}; else LP3D_COMMITTER_EMAIL=undefined; fi
     LP3D_VERSION_INFO=${lp3d_version_}" "${lp3d_revision_}" "${lp3d_git_ver_commit_count}" "${lp3d_git_ver_sha_hash_short}
 else
     Info "$((LP3D_CMD_COUNT += 1)). capture version info using input arguments"
@@ -142,6 +138,13 @@ else
     if test -n "$8"; then lp3d_suffix=$8; fi
     LP3D_VERSION_INFO=$2" "$3" "$4" "$5" "$6" "$7
 fi
+if test -z "${LP3D_COMMIT}"; then LP3D_COMMIT=`git rev-parse HEAD` >/dev/null 2>&1; fi
+if test -n "${LP3D_COMMIT}"; then 
+	lp3d_git_ver_author=`git log -1 ${LP3D_COMMIT} --pretty="%aN"` >/dev/null 2>&1
+	lp3d_git_ver_committer_email=`git log -1 ${LP3D_COMMIT} --pretty="%cE"` >/dev/null 2>&1
+fi
+if test -n "$lp3d_git_ver_author"; then LP3D_AUTHOR_NAME=${lp3d_git_ver_author}; else LP3D_AUTHOR_NAME=`echo $USER`; fi
+if test -n "$lp3d_git_ver_committer_email"; then LP3D_COMMITTER_EMAIL=${lp3d_git_ver_committer_email}; else LP3D_COMMITTER_EMAIL=undefined; fi
 if test -n "$lp3d_suffix"; then
     LP3D_VERSION_INFO="$LP3D_VERSION_INFO $lp3d_suffix"
 fi
