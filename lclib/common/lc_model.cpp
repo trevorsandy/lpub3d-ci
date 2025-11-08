63c860fa3f2a16a1ad2ac0df5ffe0897006f7908
Trevor SANDY <trevor.sandy@gmail.com>
2025-11-08 03:16:08 +0100

    LCLib: fixed duplicate action using the current step in redo.

--- lclib/common/lc_model.cpp
+++ lclib/common/lc_model.cpp
@@ -1924,7 +1924,7 @@ void lcModel::RunAddPiecesAction(const lcModelActionAddPieces* ModelActionAddPie
 	if (Apply)
 	{
 		std::vector<lcObject*> Pieces;
-		lcStep Step =  ModelActionAddPieces->GetStep();
+		lcStep Step = ModelActionAddPieces->GetStep();
 
 		for (const lcModelActionAddPieces::PieceData& PieceData : ModelActionAddPieces->GetPieceData())
 		{
@@ -2055,7 +2055,7 @@ void lcModel::RunGroupPiecesAction(const lcModelActionGroupPieces* ModelActionGr
 
 void lcModel::RecordDuplicatePiecesAction()
 {
-	std::unique_ptr<lcModelActionDuplicatePieces> ModelActionDuplicatePieces = std::make_unique<lcModelActionDuplicatePieces>();
+	std::unique_ptr<lcModelActionDuplicatePieces> ModelActionDuplicatePieces = std::make_unique<lcModelActionDuplicatePieces>(mCurrentStep);
 
 	RunDuplicatePiecesAction(ModelActionDuplicatePieces.get(), true);
 
@@ -2072,6 +2072,7 @@ void lcModel::RunDuplicatePiecesAction(const lcModelActionDuplicatePieces* Model
 		std::vector<lcObject*> NewPieces;
 		lcPiece* Focus = nullptr;
 		std::map<lcGroup*, lcGroup*> GroupMap;
+		lcStep Step = ModelActionDuplicatePieces->GetStep();
 
 		std::function<lcGroup*(lcGroup*)> GetNewGroup = [this, &GroupMap, &GetNewGroup](lcGroup* Group)
 		{
@@ -2110,7 +2111,7 @@ void lcModel::RunDuplicatePiecesAction(const lcModelActionDuplicatePieces* Model
 				continue;
 
 			lcPiece* NewPiece = new lcPiece(*Piece);
-			NewPiece->UpdatePosition(mCurrentStep);
+			NewPiece->UpdatePosition(Step);
 			NewPieces.emplace_back(NewPiece);
 
 			if (Piece->IsFocused())
