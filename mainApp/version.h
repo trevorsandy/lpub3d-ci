8127ff5f10d10ef0e95aae25246e66d386662f30
Trevor SANDY <trevor.sandy@gmail.com>
2025-11-08 03:43:04 +0100

    LCLib: added add light action.

--- mainApp/version.h
+++ mainApp/version.h
@@ -93,7 +93,7 @@
 // ~~~~~~ LeoCAD (LCLib) Version ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 #define VER_LC_VERSION_TEXT                 "25.09"
-#define VER_LC_VERSION_SHA                  "fe5df95"
+#define VER_LC_VERSION_SHA                  "c0af326"
 
 // ~~~~~~ 3rdParty Applications ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
